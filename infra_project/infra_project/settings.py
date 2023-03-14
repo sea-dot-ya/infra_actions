a857bfdfcdd0cb30d5e61b85fb9be13f83bd4243
Артем Павлов <sea.dot@yandex.ru>
2023-03-14 13:16:47 +0300

    add test with flake8 2

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+# https://docs.djangoproject.com/en/2.2/topicss/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
